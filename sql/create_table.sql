0086a7074be059351c5015796fb3082d3df4171d
h4nabii <leon_xenos.xu@foxmail.com>
2023-09-16 05:15:25 +0800

    创建了问题表与问题提交表

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -3,10 +3,10 @@
 # @from <a href="https://yupi.icu">编程导航知识星球</a>
 
 -- 创建库
-create database if not exists my_db;
+create database if not exists h4nabii_oj;
 
 -- 切换库
-use my_db;
+use h4nabii_oj;
 
 -- 用户表
 create table if not exists user
@@ -42,6 +42,28 @@ create table if not exists post
     index idx_userId (userId)
 ) comment '帖子' collate = utf8mb4_unicode_ci;
 
+-- 题目表
+create table if not exists question
+(
+    id          bigint auto_increment comment 'id' primary key,
+    title       varchar(512)                       null comment '标题',
+    content     text                               null comment '内容',
+    tags        varchar(1024)                      null comment '标签列表（JSON 数组）',
+    answer      text                               null comment '题目答案',
+    submitNum   int      default 0                 not null comment '题目提交数',
+    acceptedNum int      default 0                 not null comment '题目通过数',
+    judgeCase   text                               null comment '判题用例（JSON 数组）',
+    judgeConfig text                               null comment '判题配置（JSON 对象）',
+
+    thumbNum    int      default 0                 not null comment '点赞数',
+    favourNum   int      default 0                 not null comment '收藏数',
+    userId      bigint                             not null comment '创建用户 id',
+    createTime  datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime  datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete    tinyint  default 0                 not null comment '是否删除',
+    index idx_userId (userId)
+) comment '帖子' collate = utf8mb4_unicode_ci;
+
 -- 帖子点赞表（硬删除）
 create table if not exists post_thumb
 (
@@ -51,6 +73,24 @@ create table if not exists post_thumb
     createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
     updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     index idx_postId (postId),
+    index idx_userId (userId)
+) comment '帖子点赞';
+
+-- 题目提交表
+create table if not exists question_submit
+(
+    id         bigint auto_increment comment 'id' primary key,
+    language   varchar(128)                       not null comment '编程语言',
+    code       text                               not null comment '用户代码',
+    judgeInfo  text                               null comment '判题信息（JSON 对象）',
+    status     int      default 0                 not null comment '判题状态（0-待判题，1-判题中，2-成功，3-失败）',
+    questionId bigint                             not null comment '题目 id',
+    userId     bigint                             not null comment '创建用户 id',
+
+    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete   tinyint  default 0                 not null comment '是否删除',
+    index idx_questionId (questionId),
     index idx_userId (userId)
 ) comment '帖子点赞';
 
